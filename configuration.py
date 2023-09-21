a34295e10a322196dfc685610d9acfa1e5938134
bra1nnn <rakhmanin.alexandr@yandex.ru>
2023-09-21 09:55:39 +0300

    Добавление файлов проекта в репозиторий

---
+++ configuration.py
@@ -0,0 +1,12 @@
+# Адрес сервера
+URL_SERVICE = "https://6fc68f0d-27c6-42f8-8159-f98309316010.serverhub.praktikum-services.ru"
+
+#Api Docs
+DOC_PATH = "/docs/"
+
+#Ручка создания набора
+ADD_ORDER_PATH = "/api/v1/orders"
+
+#Ручка создания пользователя
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
+
---
+++ data.py
@@ -0,0 +1,23 @@
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+post_order_body = {
+        "firstName": "Александр",
+        "lastName": "Иванов",
+        "address": "Колпино",
+        "metroStation": 44,
+        "phone": "+7 777 777 77 77",
+        "rentTime": 4,
+        "deliveryDate": "2023-09-30",
+        "comment": "Заказ создан",
+        "color": [
+            "BLACK"
+        ]
+    }
+
+
+get_track_params = {
+    't': ''
+}
